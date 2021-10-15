618ce63bae872fff62f6712278c46e012a500fe0
Timothy Morey <timmorey@gmail.com>
2021-10-15 13:26:58 -0400

    add untilfail util

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 rsync -r home/ ~
 
+echo "" >> ~/.bashrc
 echo "# added by timmorey/dotfiles install.sh:" >> ~/.bashrc
 echo "source ~/.bashrc_colors" >> ~/.bashrc
 echo "source ~/.bashrc_git" >> ~/.bashrc
+echo "PATH=\$HOME/bin:\$PATH" >> ~/.bashrc
 echo "" >> ~/.bashrc
